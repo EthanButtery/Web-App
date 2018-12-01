eab22aebb4b64d4bdf790b59bdf44de35bf3ebf6
EC2 Default User <ec2-user@ip-172-31-10-17.ec2.internal>
2018-12-01 19:39:25 +0000

    fixed cumulative gpa and precision

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_015234) do
+ActiveRecord::Schema.define(version: 2018_11_27_000149) do
 
   create_table "courses", force: :cascade do |t|
     t.integer "semester_id"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_11_30_015234) do
     t.string "grade"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.string "user_id"
   end
 
   create_table "semesters", force: :cascade do |t|
