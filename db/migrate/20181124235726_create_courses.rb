677d6a6a6cf79ee7739e55c81d768f6f581e1513
EC2 Default User <ec2-user@ip-172-31-10-17.ec2.internal>
2018-12-02 22:53:37 +0000

    fix courses

--- db/migrate/20181124235726_create_courses.rb
+++ db/migrate/20181124235726_create_courses.rb
@@ -2,12 +2,12 @@ class CreateCourses < ActiveRecord::Migration[5.2]
   def change
     create_table :courses do |t|
       t.integer :semester_id
-      t.string :couse_name
+      t.string :course_name
       t.string :department
       t.string :instructor
       t.integer :credits
       t.string :grade
-
+      t.integer :user_id
       t.timestamps
     end
   end
